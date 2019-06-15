501c973047cbf1d4ebd1f3302d5c013c097d59ff
rbracamonte <rbracamonte@dealeron.com>
2019-06-15 16:09:19 -0300

    Work - Hide table when doesn't have data

--- js/table.js
+++ js/table.js
@@ -7,18 +7,29 @@ function insertElement() {
         foods.push(food);
 
         let table = document.querySelector("table");
-        let row = table.insertRow();
-        // let th = document.createElement("th");
-        // let text = document.createTextNode(food);
-        // th.appendChild(text);
-        // row.appendChild(th);
+        let tbody;
 
-        let cell = row.insertCell();
+        //Checks if tbody exists
+        if (!document.getElementById("tableBody")) {
+            tbody = table.createTBody();
+            tbody.id = "tableBody";
+        } else {
+            tbody = document.getElementById("tableBody");
+        }
+
+        let row = tbody.insertRow();
+        let td = document.createElement("td");
         let text = document.createTextNode(food);
-        let text2 = document.createTextNode("Soy otra columna");
-        cell.appendChild(text);
+        td.appendChild(text);
+        row.appendChild(td);
+
+        let date = new Date();
+        text = document.createTextNode(date.toDateString());
         cell = row.insertCell();
-        cell.appendChild(text2);
+        cell.appendChild(text);
+
+        //Where to put this?? 
+        document.getElementById("table").style.display = 'block';
     }
     document.getElementById("foodName").focus();
 }
@@ -31,5 +42,8 @@ document.querySelector("#foodName").addEventListener("keyup", event => {
 });
 
 window.onload = function () {
+    if (!document.getElementById("tableBody")) {
+        document.getElementById("table").style.display = 'none';
+    }
     document.getElementById("foodName").focus();
 }
